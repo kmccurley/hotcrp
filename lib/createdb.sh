13b07362182d534d61c0057015f9885443eebac5
Kevin McCurley <github@digicrime.com>
2022-08-10 23:56:23 +0000

    Try to fix again.

--- lib/createdb.sh
+++ lib/createdb.sh
@@ -394,7 +394,7 @@ if [ "$createdb" = y ]; then
     eval $MYSQLADMIN $mycreatedb_args $myargs $FLAGS --default-character-set=utf8 create $DBNAME || exit 1
 fi
 
-allhosts="localhost 127.0.0.1 localhost.localdomain$granthosts"
+allhosts="localhost"
 
 if [ "$createuser" = y ]; then
     $qecho "Creating $DBUSER user and password..."
